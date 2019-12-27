4fe92b2c03a89b5f041e54ddcab6d2ae4492dbb8
granx <729354837@qq.com>
2019-12-27 21:49:24 +0800

    redirect

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Hello World' });
+  
   // res.json({fuck:true});
-  let obj = {};
-  obj.love = 1015;
-  res.json(obj);
+  
+  // let obj = {};
+  // obj.love = 1015;
+  // res.json(obj);
+  
+  res.redirect('/api')
 });
 
 module.exports = router;
