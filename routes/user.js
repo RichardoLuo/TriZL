aff3125f925fde0dad8bd3a7244f5884da935707
granx <729354837@qq.com>
2019-12-27 09:23:18 +0800

    swagger

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+
+/**
+ * @swagger
+ * definitions:
+ *   User:
+ *     properties:
+ *       name:
+ *         type: string
+ *       age:
+ *         type: integer
+ */
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: get a user info
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: user's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: ok
+ *         schema:
+ *           $ref: '#/definitions/User'
+ */
 router.get('/:id', function(req, res, next) {
   res.json(req.params.id);
 });
