f529abee3c8742876649958a545c5c5c78f89cd8
icy <1224787558@qq.com>
2019-12-29 16:48:40 +0800

    1

--- model/BookModel.js
+++ model/BookModel.js
@@ -1,12 +1,90 @@
 exports.sql = function () {
 	/**
-	 *
+	 * 查询所有书的所有信息
+	 * @returns {string}
 	 */
-	function f() {
-	
+	function selectBookAll() {
+		return "SELECT * FROM Books";
 	}
-	
+
+	/**
+	 * 按ISBN查询书所有信息
+	 * @param ISBN
+	 * @returns {string}
+	 */
+	function selectBookISBN(ISBN) {
+		return "SELECT * FROM Books WHERE ( ISBN = '{0}' )".format(ISBN);
+	}
+
+	/**
+	 * 按书名查询书所有信息
+	 * @param name
+	 * @returns {string}
+	 */
+	function selectBookName(name) {
+		return "SELECT * FROM Books WHERE ( Name LIKE ‘%{0}%’ )".format(name);
+	}
+
+	/**
+	 * 按作者查询书所有信息
+	 * @param author
+	 * @returns {string}
+	 */
+	function selectBookAuthor(author) {
+		return "SELECT * FROM Books WHERE ( Author LIKE ‘%@Author%’ )".format(author);
+	}
+
+	/**
+	 * 新增书信息
+	 * @param ISBN
+	 * @param name
+	 * @param author
+	 * @param releasetime
+	 * @param price
+	 * @param repertory
+	 * @param introduction
+	 * @param picture
+	 * @param solenum
+	 * @returns {string}
+	 */
+	function insertBook(ISBN,name,author,releasetime,price,repertory,introduction,picture,solenum) {
+		return "INSERT INTO Books (ISBN, Name, Author, ReleaseTime, Price, Repertory, Introduction, Picture, SoleNum, AddTime)  VALUES('{0}','{1}','{2}',{3},{4},{5},'{6}','{7}',{8}, GETDATE())".format(ISBN,name,author,releasetime,price,repertory,introduction,picture,solenum);
+	}
+
+	/**
+	 * 按ISBN删除书信息
+	 * @param isbn
+	 * @returns {string}
+	 */
+	function deleteBook(isbn) {
+		return "DELETE FROM Books WHERE ISBN = '{0}'".format(isbn);
+	}
+
+	/**
+	 * 按ISBN更新书的信息
+	 * @param name
+	 * @param author
+	 * @param releasetime
+	 * @param price
+	 * @param repertory
+	 * @param introduction
+	 * @param picture
+	 * @param solenum
+	 * @param addtime
+	 * @param ISBN
+	 * @returns {string}
+	 */
+	function updateBook(name,author,releasetime,price,repertory,introduction,picture,solenum,addtime,ISBN) {
+		return "UPDATE  Books SET Name = '{0}', Author = '{1}', ReleaseTime = {2}, Price={3},  Repertory={4}, Introduction='{5}', Picture='{6}', SoleNum={7}, AddTime={8}	WHERE ISBN = '{9}'".format(name,author,releasetime,price,repertory,introduction,picture,solenum,addtime,ISBN);
+	}
+
 	return{
-		f: f,
+		selectBookAll: selectBookAll,
+		selectBookISBN: selectBookISBN,
+		selectBookName: selectBookName,
+		selectBookAuthor: selectBookAuthor,
+		insertBook: insertBook,
+		deleteBook: deleteBook,
+		updateBook: updateBook
 	}
 }
--- model/OrderModel.js
+++ model/OrderModel.js
@@ -1,12 +1,50 @@
 exports.sql = function () {
 	/**
-	 *
+	 * 按订单号查询订单所有信息
+	 * @param id
+	 * @returns {string}
 	 */
-	function f() {
-	
+	function selectOrder(id) {
+		return "SELECT * FROM Orders WHERE OrderID = {0}".format(id);
+	}
+
+	/**
+	 * 新建订单
+	 * @param OrderID
+	 * @param ISBN
+	 * @param PerchaseNum
+	 * @param CustomerName
+	 * @param CustomerPhone
+	 * @param CustomerAddr
+	 * @returns {string}
+	 */
+	function insertOrder(OrderID, ISBN, PerchaseNum, CustomerName, CustomerPhone, CustomerAddr) {
+		return "INSERT INTO Orders  (OrderID, OrderTime, ISBN, PerchaseNum, CustomerName, CustomerPhone, CustomerAddr, OrderStatus)  VALUES  ({0}, GETDATE(), {1}, {2}, {3}, {4}, {5}, 0);".format(OrderID, ISBN, PerchaseNum, CustomerName, CustomerPhone, CustomerAddr);
+	}
+
+	/**
+	 * 按订单号删除订单
+	 * @param id
+	 * @returns {string}
+	 */
+	function deleteOrder(id) {
+		return "DELETE FROM Orders WHERE OrderID = {0}".format(id);
+	}
+
+	/**
+	 * 更新订单运输状态
+	 * @param state
+	 * @param id
+	 * @returns {string}
+	 */
+	function updateOrder(state,id) {
+		return "UPDATE Orders SET  OrderState = {0} WHERE  (OrderID = {1})".format(state,id);
 	}
 	
 	return{
-		f: f,
+		selectOrder: selectOrder,
+		insertOrder: insertOrder,
+		deleteOrder: deleteOrder,
+		updateOrder: updateOrder
 	}
 }
